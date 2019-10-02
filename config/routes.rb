0a38cf0d41279ad3f98b666b00043badb241ea20
Ivan Lopatin <gingray.dev@gmail.com>
2019-10-02 13:28:50 +0200

    add web sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
+require 'sidekiq/web'
+require 'sidekiq/cron/web'
+
 Rails.application.routes.draw do
   root 'home#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  mount Sidekiq::Web, at: '/sidekiq'
 end
